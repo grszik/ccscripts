a79875516a0e73b4b910b3fe790c2b87c5dde423
grosik <99998467+grszik@users.noreply.github.com>
2025-05-12 18:37:42 +0200

    Update email.lua

--- email.lua
+++ email.lua
@@ -5,9 +5,34 @@ local monitor = peripheral.wrap("top")
 monitor.clear()
 
 local funs = {}
+local x = {}
 
+function encrypt(str)
+    local count = 0
+    for i=1,str:len() do
+        count = count + str:sub(i,i):byte()
+    end
+    return count
+end
+function check(user, pass)
+    local folder = fs.combine(path,user)
+    if not fs.exists(folder) and user:len() > 2 and folder ~= path then return false end
+    local file = fs.open(fs.combine(folder,"password"),"r").readLine()
+    if file == pass return true
+    else return false end
+end
+function register(user, pass)
+    local folder = fs.combine(path,user)
+    if fs.exists(folder) or user:len() > 2 or folder ~= path then return false end
+    local file = fs.open(fs.combine(folder,"password"),"w")
+    file.write(encrypt(pass))
+end
+        
 function button(text,color,fun,line)
-    if funs[line] == nil then funs[line] = {} end
+    if funs[line] == nil then 
+        funs[line] = {} 
+        x[line] = {} 
+    end
     local w,h = monitor.getSize()
     startX = x[line]
     monitor.setTextColor(color)
@@ -19,3 +44,5 @@ function button(text,color,fun,line)
         funs[line][i] = fun
     end
 end
+
+term.write(encrypt("banana")
