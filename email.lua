95dd89ed20a3e3ff61ee6dc9480cc27564d59736
grosik <99998467+grszik@users.noreply.github.com>
2025-05-12 20:19:42 +0200

    Update email.lua

--- email.lua
+++ email.lua
@@ -5,7 +5,8 @@ local monitor = peripheral.wrap("top")
 monitor.clear()
 
 local modem = peripheral.wrap("front")
-modem.open(9100)
+local port = 9100
+modem.open(port)
 
 local funs = {}
 local x = {}
@@ -30,13 +31,17 @@ function encrypt(str)
 end
 function check(user, pass)
     local folder = fs.combine(path,user)
-    if not fs.exists(folder) and user:len() > 2 and folder ~= path then return false end
+    if not fs.exists(folder) or user:len() < 2 or folder == path then return false end
     local file = fs.open(fs.combine(folder,"password"),"r").readLine()
     if file == pass then
         return true
     else 
         return false
     end
+end
+function exists(user)
+    local folder = fs.combine(path,user)
+    return fs.exists(folder) and user:len() > 2 and folder ~= path
 end
 function register(user, pass)
     local folder = fs.combine(path,user)
@@ -47,9 +52,22 @@ function register(user, pass)
     fs.makeDir(fs.combine(folder,"sent"))
 end
 
-function send(user,pass,receiver)
-    if not check(user,pass) then return false end
-    return true
+function send(user,pass,receiver,res, message)
+    if not check(user,pass) then 
+        modem.transmit(res,port,"no-permission")
+    elseif not exists(receiver) then
+        modem.transmit(res,port,"unknown-receiver")
+    elseif message:len() < 1 then
+        modem.transmit(res,port,"no-message")
+    end
+
+    local rdirf = fs.combine(path,user,"sent",os.date())
+    local sdirf = fs.combine(path,receiver,"got",os.date())
+    
+    local rfile = fs.open(rdirf,"w")
+    rfile.write("To: " .. receiver .. "\nAt (GMT): " .. os.date() .. "Message:\n" .. message)
+    local sfile = fs.open(sdirf,"w")
+    sfile.write("From: " .. user .. "\nAt (GMT): " .. os.date() .. "Message:\n" .. message)
 end
         
 function button(text,color,fun,line)
@@ -79,7 +97,7 @@ end
 function mwrite(text)
   local w,h = monitor.getSize()
   if #lines < h-5 then
-    lines[#lines+1 = text
+    lines[#lines+1] = text
     monitor.setCursorPos(1,#lines)
     monitor.clearLine()
     monitor.write(text)
@@ -102,12 +120,26 @@ while true do
     if message:match("^register-") then
         local params = {}
         for p in message:gmatch("([^%-]+)") do table.insert(params, p) end
-        local user = params[1]
+        local user = params[2]
         mwrite("u: " .. user)
-        local pass = params[2]
+        local pass = params[3]
         mwrite("p: " .. pass)
+
+        local worked = register(user,pass)
+        modem.transmit(replyChannel,channel,worked)
+    elseif message:match("^send-") then
+        local params = {}
+        for p in message:gmatch("([^%-]+)") do table.insert(params, p) end
+        local user = params[2]
+        local pass = params[3]
+        local to = params[4]
+        local length = 8+user:len()+pass:len()+to:len()
+        local data = message:sub(data,-1)
+        mwrite(("User %s is trying to message %s."):format(user,to))
+
+        send(user,pess,to,replyChannel,data)
     end
-    mwrite(("Message received on side %s on channel %d (reply to %d) from %f blocks away with message %s"):format(
-        side, channel, replyChannel, distance, tostring(message)
+    mwrite(("Message: %s"):format(
+        tostring(message)
     ))
 end
